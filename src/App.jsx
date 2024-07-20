a63b28618f2c85b313cf803de281d0a8f9ca1333
Duqowdev <liibaanosman779@gmail.com>
2024-07-20 10:06:16 +0100

    waxaan wax kabalay app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
+
 import Header from './Components/header/Header'
 import Date from './Components/Date/Date'
+import React from 'react'
 
-
-// this is the app function
+// this is the app functionxccnkcn
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
